6ca74940c21a8816cb7fb9ea45f7298ca78fb5da
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-10 20:45:40 -0300

    Create friends.py

---
+++ friends.py
@@ -0,0 +1,11 @@
+"""
+Store the names of a few of your friends in a list called names . 
+Print each person’s name by accessing each element in the list, one at a time 
+"""
+
+friends = ["Kat", "And", "Gra", "Mer", "Adri", "Fra"]
+n = 0
+
+while n < len(friends):
+    print(f"Hello {friends[n]}. Have a nice day!!!!")
+    n +=1
