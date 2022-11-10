6d58a6481fe378be814da9ce21ec6cd9afff28f9
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-10 20:45:48 -0300

    Create motorcycle.py

---
+++ motorcycle.py
@@ -0,0 +1,30 @@
+motorcycle = []
+
+motorcycle.append('Honda')
+motorcycle.append('Yamaha')
+
+print(motorcycle)
+
+
+"""
+other example
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'ducati') 
+
+print(motorcycles)
+____________________________
+example to remove itens of a list
+
+motorcycles = ['honda', 'yamaha', 'suzuki'] 
+
+del motorcycles[0]          #remove honda
+print(motorcycles)
+
+-----
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati') 
+print(motorcycles)
+
+
+"""
