455f01a87e1b4899e8360786669e18f318e5d8cb
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-10 20:45:19 -0300

    Create cars.py

---
+++ cars.py
@@ -0,0 +1,15 @@
+#sort the itens definitely
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort() 
+print(cars)
+
+
+"""
+cars = ['bmw', 'audi', 'toyota', 'subaru'] 
+cars.sort(reverse=True)      #   to do in the reverse order
+print(cars)
+
+
+
+#if you use the sort() just in print, the list remains the same
+"""
