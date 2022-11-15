81993b924c42a58e41d868d35a529fbea77031dd
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-15 17:33:12 -0300

    Create aliens2.py

---
+++ aliens2.py
@@ -0,0 +1,6 @@
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
+
+#Removing Key-Value Pairs                   the deleted key-value pair is removed permanently.
+del alien_0['points']
+print(alien_0)
