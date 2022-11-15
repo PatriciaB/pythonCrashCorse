2d34d871ca55dbbd0ce159d0eb370504a60b834f
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-15 19:13:10 -0300

    Create favorite_places.py

---
+++ favorite_places.py
@@ -0,0 +1,16 @@
+"""
+Make a dictionary called favorite_places. Think of three names to use as keys in the dictionary, 
+and store one to three favorite places for each person . To make this exercise a bit more interesting, 
+ask some friends to name a few of their favorite places . 
+Loop through the dictionary, and print each person’s name and their favorite places.
+"""
+
+favorite_places = {
+    'adriano' : ['bologna', 'verona'],
+    'patricia' : ['venice', 'roma'],
+}
+
+for name, places in favorite_places.items():
+    print(f"{name.title()} your favorites places are:")
+    for place in places:
+        print(f"\t{place.title()}")
