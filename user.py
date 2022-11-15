111f390c816c5b3b2cee5b88e41817d324f05e46
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-15 17:33:29 -0300

    Create user.py

---
+++ user.py
@@ -0,0 +1,8 @@
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico', 
+    'last': 'efermi',
+}
+
+for key, value in user_0.items():
+    print(f"Key: {key.title()}, Value: {value.title()}")
