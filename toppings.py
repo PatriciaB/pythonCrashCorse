55d3fb7e90b239bc3c0aab2632f88cfec5f04c3b
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-15 13:17:48 -0300

    Create toppings.py

---
+++ toppings.py
@@ -0,0 +1,8 @@
+"""
+We’ll store a requested pizza topping in a variable and then print a message if the person did not order anchovies
+"""
+
+request_topping = 'mushrooms'
+
+if request_topping != 'anchovies':
+    print("Hold the anchovies!")
