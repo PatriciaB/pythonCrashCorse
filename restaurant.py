cfbd7e189334e768380c9dbfa8f7f4c8cbed7ecb
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-15 22:22:33 -0300

    Create restaurant.py

---
+++ restaurant.py
@@ -0,0 +1,12 @@
+"""
+Write a program that asks the user how many people are in their dinner group . 
+If the answer is more than eight, print a message say- ing they’ll have to wait for a table . 
+Otherwise, report that their table is ready
+"""
+
+
+dinner_group = int(input("How many people are in your dinner group? "))
+if dinner_group > 8:
+    print("You'll have to wait.")
+else:
+    print("Your table is ready.")
