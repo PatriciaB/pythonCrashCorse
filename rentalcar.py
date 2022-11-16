9d5f34dfb07b74eccffe34cadb347cf2059294f1
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-15 22:22:31 -0300

    Create rentalcar.py

---
+++ rentalcar.py
@@ -0,0 +1,8 @@
+"""
+Write a program that asks the user what kind of rental car they would like . 
+Print a message about that car, such as “Let me see if I can find you a Subaru .
+”"""
+
+car = input("What kind of rental car do you like? ")
+
+print(f"Let me see if I can find you a {car.title()}")
