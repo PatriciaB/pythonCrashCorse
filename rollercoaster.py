2e054ebf7cced016f24db6f9746b6b2a31e0e04d
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-15 22:22:35 -0300

    Create rollercoaster.py

---
+++ rollercoaster.py
@@ -0,0 +1,7 @@
+#program that determines whether people are tall enough to ride a roller coaster
+height = int(input("How tall are you, in inches? "))
+
+if height >= 36:
+    print("\nYou're tall enough to ride!")
+else:
+    print("\nYou'll be able to ride when you're a little older.")
