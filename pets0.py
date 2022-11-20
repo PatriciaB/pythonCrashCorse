05861dec880a018f53509cef586ad0c85bafba96
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-19 21:05:28 -0300

    Create pets0.py

---
+++ pets0.py
@@ -0,0 +1,9 @@
+#to remove the value 'cat' if its repeated several times.
+
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+
+while 'cat' in pets:
+    pets.remove('cat')
+
+print(pets)
