c55adc26d6d17b9005c84631eaea55bb35def61d
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2022-11-19 21:15:37 -0300

    Create sandwich_orders.py

---
+++ sandwich_orders.py
@@ -0,0 +1,19 @@
+"""
+Make a list called sandwich_orders and fill it with the names of various sandwiches . 
+Then make an empty list called finished_sandwiches . 
+Loop through the list of sandwich orders and print a message for each order, such as I made your tuna sandwich. 
+As each sandwich is made, move it to the list of finished sandwiches . 
+After all the sandwiches have been made, print a message listing each sandwich that was made 
+"""
+
+sandwich_orders = {'turkey', 'cheese', 'chicken', 'tuna'}
+finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwiches = sandwich_orders.pop()
+    print(f"I made your {current_sandwiches.title()} sandwich") 
+    finished_sandwiches.append(current_sandwiches)
+
+print("\nThe finished sandwiches are: ")
+for sandwich in finished_sandwiches:
+    print(sandwich.title())
