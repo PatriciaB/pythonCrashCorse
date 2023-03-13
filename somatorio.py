2ed822fb056c3243a3dc47c8c2ff0bff7967c644
PatriciaB <patriciabortoluzzipiaui@gmail.com>
2023-03-12 22:48:16 -0300

    Create somatorio.py

---
+++ somatorio.py
@@ -0,0 +1,22 @@
+"""
+5. Crie um código que realize o somatório de todos os caracteres da seguinte
+string:
+Para resolver este problema, considere as seguintes dicas:
+● A soma deverá ser 1 + 2 + 7 + 9 + 5 + 7 = 31;
+● Utilize o laço de repetição for … in; para percorrer cada caractere da string;
+● Utilize a conversão do tipo string para o tipo inteiro (int(caractere)) para
+converter os caracteres em valores numéricos;
+● Utilize uma variável auxiliar, soma, para acumular o somatório dos valores.
+"""
+
+numero = '127957'
+soma = 0
+
+print(int(numero)+1.0
+)
+for i in numero:
+    soma = int(i) + soma
+
+print(soma)
+
+
